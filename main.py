61e9ec3d5428648cacb6abb6e5a509faeb260945
Klaus <hallo@klauskeusch.de>
2022-10-05 14:52:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ for arguments in args:
 videofile = "{}".format(args[0])
 
 cap = cv2.VideoCapture(videofile)
-img = cv2.imread("K20-30-0,95_1.jpg")
+# img = cv2.imread("K20-30-0,95_1.jpg")
 start_time = datetime.now()
 i = 0
 z = 0
@@ -46,137 +46,146 @@ list1 = [0, 0] * 1000
 list2 = [0] * 1000
 s = 0
 
+
+
+
+
 while True:
-    _, img = cap.read()
+    #_, img = cap.read()
+    ret, img = cap.read()
+    if ret: 
 
-    # Convert Image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Convert Image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Mask Area of Interest
-    mask2 = np.zeros(gray.shape, np.uint8)
-    cv2.circle(mask2, (555, 390), 230, (255, 255, 255), -1)
+        # Mask Area of Interest
+        mask2 = np.zeros(gray.shape, np.uint8)
+        cv2.circle(mask2, (555, 390), 230, (255, 255, 255), -1)
 
-    res = cv2.bitwise_and(gray, gray, mask=mask2)
+        res = cv2.bitwise_and(gray, gray, mask=mask2)
 
-    # Create a threshold
-    ret, thresh2 = cv2.threshold(res, 100, 155, cv2.THRESH_BINARY)
+        # Create a threshold
+        ret, thresh2 = cv2.threshold(res, 100, 155, cv2.THRESH_BINARY)
 
-    cv2.circle(thresh2, (555, 390), 2, (255, 255, 255), 2)
+        cv2.circle(thresh2, (555, 390), 2, (255, 255, 255), 2)
 
-    contours, heirarchy = cv2.findContours(
-        thresh2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
-    )
-
-    largest_areas = sorted(contours, key=cv2.contourArea)
-
-    mask = np.zeros(gray.shape, np.uint8)
-
-    bounding = cv2.boundingRect(largest_areas[-1])
-    (x1, y1, x2, y2) = bounding
-
-    img_contour = cv2.drawContours(
-        mask, [largest_areas[-1]], 0, (255, 255, 255, 255), 1
-    )
-
-    fps = cap.get(cv2.CAP_PROP_FPS)  # OpenCV v2.x used "CV_CAP_PROP_FPS"
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = frame_count / fps
-    minutes = int(duration / 60)
-    seconds = duration % 60
-
-    if keyboard.is_pressed("Spacebar"):
-        list1[i] = (x2, y2)
-        list2[i] = y2
-        i = i + 1
-
-    time_elapsed = datetime.now() - start_time
-
-    z = int(time_elapsed.seconds) - s
-    if z > 10:
-        s = s + 10
-        # print("jetzt")
-
-    # Draw boundaries
-    for cnt in contours:
-        # Draw polgon
-        img_polylines = cv2.polylines(img, [largest_areas[-1]], True, (255, 0, 0), 3)
-
-        cv2.rectangle(img, bounding, (0, 180, 180), 2)
-
-        cv2.putText(
-            img,
-            "Meltpoollaenge: {}".format(round(x2, 1)),
-            (100, 50),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
-        )
-        cv2.putText(
-            img,
-            "Meltpoolbreite: {}".format(round(y2, 1)),
-            (100, 80),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
-        )
-        cv2.putText(
-            img,
-            "Duration = " + str(minutes) + ":" + str(round(seconds, 2)),
-            (100, 740),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
-        )
-        cv2.putText(
-            img,
-            "Time elapsed = {}".format(time_elapsed),
-            (100, 710),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
-        )
-        cv2.putText(
-            img,
-            "Time elapsed = {}".format("%s" % (time_elapsed.seconds)),
-            (100, 680),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
-        )
-        cv2.putText(
-            img,
-            "Counter = {}".format(i),
-            (800, 680),
-            cv2.FONT_HERSHEY_PLAIN,
-            1.5,
-            (0, 0, 255),
+        contours, heirarchy = cv2.findContours(
+            thresh2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
         )
 
-        # y2 Meltpoolbreite
-    data = {
-        y2,
-        x2
-    }
+        largest_areas = sorted(contours, key=cv2.contourArea)
 
-    df = pd.DataFrame(data)
-    print(df)
+        mask = np.zeros(gray.shape, np.uint8)
 
-    # print("{}".format(y2))
+        bounding = cv2.boundingRect(largest_areas[-1])
+        (x1, y1, x2, y2) = bounding
 
-    # cv2.imshow("Gray", gray)
-    # cv2.imshow("thresh2", thresh2)
-    # cv2.imshow("Result_Mask", res)image.png
-    # cv2.imshow("Rect", img_rect)
-    # cv2.imshow("Polylines", img_polylines)
-    # cv2.imshow("thresh3", thresh3)
-    # cv2.imshow("kontur", img_contour)
-    # cv2.imshow("thresh", thresh)
-    cv2.imshow("Image", img)
+        img_contour = cv2.drawContours(
+            mask, [largest_areas[-1]], 0, (255, 255, 255, 255), 1
+        )
 
-    key = cv2.waitKey(1)
+        fps = cap.get(cv2.CAP_PROP_FPS)  # OpenCV v2.x used "CV_CAP_PROP_FPS"
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = frame_count / fps
+        minutes = int(duration / 60)
+        seconds = duration % 60
 
-    if key == ord('q'):
-        break
+        if keyboard.is_pressed("Spacebar"):
+            list1[i] = (x2, y2)
+            list2[i] = y2
+            i = i + 1
+
+        time_elapsed = datetime.now() - start_time
+
+        z = int(time_elapsed.seconds) - s
+        if z > 10:
+            s = s + 10
+            # print("jetzt")
+
+        # Draw boundaries
+        for cnt in contours:
+            # Draw polgon
+            img_polylines = cv2.polylines(img, [largest_areas[-1]], True, (255, 0, 0), 3)
+
+            cv2.rectangle(img, bounding, (0, 180, 180), 2)
+
+            cv2.putText(
+                img,
+                "Meltpoollaenge: {}".format(round(x2, 1)),
+                (100, 50),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+            cv2.putText(
+                img,
+                "Meltpoolbreite: {}".format(round(y2, 1)),
+                (100, 80),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+            cv2.putText(
+                img,
+                "Duration = " + str(minutes) + ":" + str(round(seconds, 2)),
+                (100, 740),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+            cv2.putText(
+                img,
+                "Time elapsed = {}".format(time_elapsed),
+                (100, 710),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+            cv2.putText(
+                img,
+                "Time elapsed = {}".format("%s" % (time_elapsed.seconds)),
+                (100, 680),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+            cv2.putText(
+                img,
+                "Counter = {}".format(i),
+                (800, 680),
+                cv2.FONT_HERSHEY_PLAIN,
+                1.5,
+                (0, 0, 255),
+            )
+
+            # y2 Meltpoolbreite
+        data = {
+            round(x2, 1),
+            round(y2, 1),
+            #y2,
+            #x2
+        }
+        # testvar = round(y2, 1)
+        # print("Testvar: {}".format(testvar))
+        df = pd.DataFrame(data)
+        print(df)
+
+        # print("{}".format(y2))
+
+        # cv2.imshow("Gray", gray)
+        # cv2.imshow("thresh2", thresh2)
+        # cv2.imshow("Result_Mask", res)image.png
+        # cv2.imshow("Rect", img_rect)
+        # cv2.imshow("Polylines", img_polylines)
+        # cv2.imshow("thresh3", thresh3)
+        # cv2.imshow("kontur", img_contour)
+        # cv2.imshow("thresh", thresh)
+        cv2.imshow("Image", img)
+
+        key = cv2.waitKey(1)
+
+        if key == ord('q'):
+            break
 
 # print(list1)
 
