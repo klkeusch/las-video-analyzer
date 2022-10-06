862aed9f3182e0e9b248fe2f4150e02e707dbe42
Klaus <hallo@klauskeusch.de>
2022-10-06 09:04:54 +0200

    Update main.py
    update variables in text positioning

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import pandas as pd
 import matplotlib.image as mpimg
 import openpyxl
 import time
+
 # from timer import Timer
 from datetime import datetime
 import keyboard
@@ -29,9 +30,10 @@ for arguments in args:
     print(arguments)
 
 videofile = "{}".format(args[0])
+print("Datei: {}".format(videofile))
 
-#img = cv2.imread("K20-30-0,95_1.jpg")
-#cap = cv2.VideoCapture('22-30-0,95.mp4')
+# img = cv2.imread("K20-30-0,95_1.jpg")
+# cap = cv2.VideoCapture('22-30-0,95.mp4')
 # cap = cv2.VideoCapture('testvideo.mp4')
 # cap = cv2.VideoCapture('C:/Users/sotto/PycharmProjects/IP_Laser_pycv/testvideo.mp4')
 cap = cv2.VideoCapture(videofile)
@@ -48,21 +50,24 @@ list2 = [0] * 1000
 
 s = 0
 
+position_text_left_side = 25
+position_text_left_side_top_y = 25
+position_text_left_side_bottom_y = 680
+
+position_text_right_side = 780
+position_text_right_side_bottom = 100
+
 while True:
     ret, img = cap.read()
 
-    if not cap.isOpened():
-        print("No Input")
-        break
-
     if not ret:
-        print("End of File?")
+        print("End of File")
         break
 
     # Convert Image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #Mask Area of Interest
+    # Mask Area of Interest
     mask2 = np.zeros(gray.shape, np.uint8)
     cv2.circle(mask2, (555, 390), 230, (255, 255, 255), -1)
 
@@ -71,12 +76,11 @@ while True:
     # Create a threshold
     ret, thresh2 = cv2.threshold(res, 100, 155, cv2.THRESH_BINARY)
 
-
-
     cv2.circle(thresh2, (555, 390), 2, (255, 255, 255), 2)
 
-
-    contours, heirarchy = cv2.findContours(thresh2 ,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, heirarchy = cv2.findContours(
+        thresh2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+    )
 
     largest_areas = sorted(contours, key=cv2.contourArea)
 
@@ -85,7 +89,9 @@ while True:
     bounding = cv2.boundingRect(largest_areas[-1])
     (x1, y1, x2, y2) = bounding
 
-    img_contour = cv2.drawContours(mask, [largest_areas[-1]], 0, (255, 255, 255, 255), 1)
+    img_contour = cv2.drawContours(
+        mask, [largest_areas[-1]], 0, (255, 255, 255, 255), 1
+    )
 
     fps = cap.get(cv2.CAP_PROP_FPS)  # OpenCV v2.x used "CV_CAP_PROP_FPS"
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -93,62 +99,97 @@ while True:
     minutes = int(duration / 60)
     seconds = duration % 60
 
-
-
-    #if keyboard.is_pressed('Spacebar'):
-
-        # list1[i] = (x2, y2)
-        # list2[i] = y2
-        # i = i + 1
-
-
-    # INSERT YOUR CODE
-
     time_elapsed = datetime.now() - start_time
-    #print ('%s' % (time_elapsed.seconds))
 
     z = int(time_elapsed.seconds) - s
     if z > 1:
         s = s + 1
-        print('jetzt')
 
         list1[i] = (x2, y2)
         list2[i] = y2
         i = i + 1
+        print("Measuring...({})".format(i))
 
-
-
-    #Draw boundaries
+    # Draw boundaries
     for cnt in contours:
-        #Draw polgon
+        # Draw polgon
         img_polylines = cv2.polylines(img, [largest_areas[-1]], True, (255, 0, 0), 3)
 
-
         cv2.rectangle(img, bounding, (0, 180, 180), 2)
-        #cv2.circle(img, intcenter, intradius, (0, 0, 255), 2)
-        cv2.putText(img,"Meltpoollaenge: {}".format(round(x2,1)), (100, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (0,0,255))
-        cv2.putText(img, "Meltpoolbreite: {}".format(round(y2,1)), (100, 80), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255))
-        cv2.putText(img, 'Duration = ' + str(minutes) + ':' + str(round(seconds, 2)), (100, 740), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255))
-        cv2.putText(img, 'Time elapsed = {}'.format(time_elapsed), (100, 710), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255))
-        cv2.putText(img, 'Time elapsed = {}'.format('%s' % (time_elapsed.seconds)), (100, 680), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255))
-        cv2.putText(img, 'Counter = {}'.format(i), (800, 680), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255))
 
+        cv2.putText(
+            img,
+            "Stop measurement: press q",
+            (position_text_left_side , position_text_left_side_top_y),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
 
-
+        cv2.putText(
+            img,
+            "Meltpoollaenge: {}".format(round(x2, 1)),
+            (position_text_left_side, position_text_left_side_top_y + 35),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
+        cv2.putText(
+            img,
+            "Meltpoolbreite: {}".format(round(y2, 1)),
+            (position_text_left_side, position_text_left_side_top_y + 60),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
+        cv2.putText(
+            img,
+            "Duration = " + str(minutes) + ":" + str(round(seconds, 2)),
+            (position_text_right_side, position_text_left_side_bottom_y + 30),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
+        cv2.putText(
+            img,
+            "Time elapsed = {}".format(time_elapsed),
+            (position_text_left_side, position_text_left_side_bottom_y + 30),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
+        cv2.putText(
+            img,
+            "Time elapsed (sec) = {}".format("%s" % (time_elapsed.seconds)),
+            (position_text_left_side, position_text_left_side_bottom_y),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
+        cv2.putText(
+            img,
+            "Counter = {}".format(i),
+            (position_text_right_side, position_text_left_side_bottom_y),
+            cv2.FONT_HERSHEY_PLAIN,
+            1.5,
+            (0, 0, 255),
+        )
 
     cv2.imshow("Image", img)
 
-
     key = cv2.waitKey(1)
 
-    if key == ord('q'):
+    if key == ord("q"):
+        print("Abort - stop key pressed")
         break
 
-print(list1)
-
-with open("out.csv", "w", newline="") as f:
+csv_filename = "{}.csv".format(videofile)
+# with open("{}.csv".format(videofile), "w", newline="") as f:
+with open(csv_filename, "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(list1)
+
+print("Measurements saved in file {}".format(csv_filename))
 
 cap.release()
 cv2.destroyAllWindows()
